4aa54772454af49cff09db0d06f7e65ba5864623
Ayush Kumar Gupta <67339899+imayush18@users.noreply.github.com>
2022-07-03 09:10:46 +0530

    Time: 16 ms (15.26%), Space: 16.5 MB (19.50%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    void helper(vector<int> nums, set<string> &s, int i, int N, string x){
+        if(i==N){
+            if(x==""){
+                string a="ayush";
+                s.insert(a);  
+                
+            }
+            
+            else{
+                sort(x.begin(), x.end());
+                s.insert(x);
+            }
+            
+            return;
+        }
+        
+        helper(nums, s, i+1, N, x);
+        helper(nums, s, i+1, N, x+=nums[i]+'0');
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<string> s;
+        
+        string x="";
+        helper(nums, s, 0, nums.size(), x);
+        
+        vector<vector<int>> ans;
+        for(auto i:s){
+            vector<int> v;
+           if(i=="ayush"){
+               ans.push_back(v);
+           }
+            else{
+                int a=0;
+                int len=i.length();
+                for(int j=0; j<len; j++){
+                    v.push_back(i[j]-'0');
+                }
+                
+                ans.push_back(v);
+            }
+        }
+        
+        for(int z=0; z<ans[0].size(); z++){
+            sort(ans[z].begin(), ans[z].end());
+        }
+        // sort(ans)
+        return ans;
+    }
+};
