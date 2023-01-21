3f3a0f7f1bc2deef1862ebd98f52e02168fed196
Ayush Kumar Gupta <67339899+imayush18@users.noreply.github.com>
2023-01-21 22:19:38 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -1,45 +1,52 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
   public:
+  
+    
+    bool helper(vector<bool> &vis, vector<bool> &dfsvis, int node, vector<int> adj[]){
+        
+        vis[node]=dfsvis[node]=true;
+        
+        for(auto x: adj[node]){
+            if(!vis[x]){
+                if(helper(vis, dfsvis, x, adj))
+                    return true;
+            }
+            else{
+                if(dfsvis[x])
+                    return true;
+            }
+        }
+        dfsvis[node]=false;
+        return false;
+        
+    }
+    
+  
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
+       
+        vector<bool> vis(V, false);
+        vector<bool> dfsvis(V, false);
+       
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(helper(vis, dfsvis, i, adj)){
+                    return true;
+                }
+            }
+        }
         
-        vector<int> indegree(V, 0);
-	    for(int i=0; i<V; i++){
-	        for(auto x: adj[i]){
-	            indegree[x]++;
-	        }
-	    }
-	    
-	    queue<int> q;
-	    for(int i=0; i<V; i++){
-	        if(indegree[i]==0)
-	            q.push(i);
-	    }
-	    
-	    int cnt=0;;
-	    while(!q.empty()){
-	        int node=q.front();
-	        q.pop();
-	        cnt++;;
-	        
-	        for(auto x: adj[node]){
-	            indegree[x]--;
-	            if(indegree[x]==0)
-	                q.push(x);
-	        }
-	    }
-        if(cnt==V)
-            return false;
-        return true;
+        return false;
+       
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
 
@@ -63,4 +70,5 @@ int main() {
 
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
